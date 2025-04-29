594cc0a82743fa709f92b23762587450dc9e2659
Mikhael D'Amato <damato578@gmail.com>
2025-04-29 07:19:44 -0300

    feat: add web-service

---
+++ src/br/com/damatomos/currency_converter/infrastructure/services/WebService.java
@@ -0,0 +1,20 @@
+package br.com.damatomos.currency_converter.infrastructure.services;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
+public class WebService<T> {
+
+    public static String get(String url) throws IOException, InterruptedException {
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder(URI.create(url)).build();
+
+        HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+
+        return response.body();
+    }
+
+}
